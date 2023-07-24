548706af51533f81e970eaaeea3ca048ee9eac0f
sasmeka <verdysas@gmail.com>
2023-07-24 21:04:52 +0700

    update utils send email

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,9 +1,6 @@
 const nodemailer = require('nodemailer')
 const sendMail = (email, subject, text) => {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
         service: 'gmail',
         auth: {
             user: process.env.guser,
